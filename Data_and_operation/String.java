2cf892e0f5dbd2ea2fc978ce2074263693185e11
Young Eun Kim <105777568+2022shushu@users.noreply.github.com>
2023-05-09 21:57:41 +0900

    Add files via upload

---
+++ Data_and_operation/String.java
@@ -0,0 +1,16 @@
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.println('H');
+		System.out.println("H");
+		
+		System.out.println("Hello"
+				+ "World");
+		System.out.println("Hello \nWorld");
+		System.out.println("Hello \"World\"");
+	}
+
+}
